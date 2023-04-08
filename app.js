29fc219deb15348dff5e0acc481bb91c709b63fb
amarni-mohammad <122675946+amarni-mohammad@users.noreply.github.com>
2023-04-08 20:12:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let img =document.querySelector('.img');
+let conunter =document.querySelector('.conunter');
+function phones (iphone){
+    img.src=iphone;
+}
+function colors (color){
+    conunter.style.background=color;
+}
